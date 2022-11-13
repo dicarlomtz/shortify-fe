795a3a11a5cfb1fa1dfc109e12eea9094c1d22d7
dicarlomtz <dicarlomtz@gmail.com>
2022-11-12 23:02:32 -0600

    Adding shortify app component with app theme and router

---
+++ src/ShortifyApp.jsx
@@ -0,0 +1,12 @@
+import AppRouter from './router'
+import AppTheme from './theme'
+
+export const ShortifyApp = () => {
+  return (
+    <AppTheme>
+        <AppRouter />
+    </AppTheme>
+  )
+}
+
+export default ShortifyApp
