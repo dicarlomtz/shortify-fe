59c1795cb3a0a890cdad434a591537cc973cb251
dicarlomtz <dicarlomtz@gmail.com>
2022-11-12 23:33:32 -0600

    Using navigation bar

--- src/ui/layout/PageContentLayout.jsx
+++ src/ui/layout/PageContentLayout.jsx
@@ -1,9 +1,11 @@
 import Container from '@mui/material/Container'
 import PropTypes from 'prop-types'
+import { Navbar } from '../../navigation'
 
 export const PageContentLayout = ({ children }) => {
   return (
       <>
+        <Navbar />
         <Container maxWidth='lg' sx={{ pt: 5 }}>
             { children }
         </Container>
