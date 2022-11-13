866f0195e8395cac4cc47c58a6b47f57d879823a
dicarlomtz <dicarlomtz@gmail.com>
2022-11-12 23:02:59 -0600

    Adding shortify app component to main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import ShortifyApp from './ShortifyApp'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
-
+  <BrowserRouter>
+    <ShortifyApp />
+  </BrowserRouter>
 )
